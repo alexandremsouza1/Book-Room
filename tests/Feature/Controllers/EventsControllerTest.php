46245ec5ba3d191b749b1e3a85ed1bda9c892adc
Alexandre Magno <xandemag@gmail.com>
2024-09-21 18:44:16 -0300

    test: Event test

--- tests/Feature/Controllers/EventsControllerTest.php
+++ tests/Feature/Controllers/EventsControllerTest.php
@@ -46,27 +46,27 @@ class EventsControllerTest extends TestCase
     public function testCreateEvent()
     {
         $now = Carbon::now();
-        $now = Carbon::now();
-        $startTime = $now->format('Y-m-d H:i:s');
-        $endTime = $now->addHour()->format('Y-m-d H:i:s');
+        $startTime = Carbon::createFromFormat('d/m/Y', '01/09/2024')->format('Y-m-d H:i');
+        $endTime = Carbon::createFromFormat('d/m/Y', '01/09/2024')->format('Y-m-d H:i');
+        $recurring_until = $now->addHour()->format('Y-m-d');
 
         $token = Str::random(40);
 
         $params = [
-            '_method' => 'POST',
-            '_token' => $token,
+            '_method' => 'POST', 
+            '_token' => $token , // Obtém o token CSRF
             'room_id' => 1,
             'user_id' => 1,
-            'title' => 'book test',
-            'start_time' => $startTime,
-            'end_time' => $endTime,
-            'description' => 'description test'
+            'title' => 'sfdasd',
+            'start_time' =>  $startTime, // Formato correto para o Laravel
+            'end_time' => $endTime,   // Formato correto para o Laravel
+            'description' => 'dsafsd',
+            'recurring_until' => '2024-01-09', 
         ];
 
         // Faz a requisição POST
         $response = $this->post('/admin/events', $params);
 
-        // Verifica se a resposta é 302 (Redirecionamento)
         $response->assertStatus(302);
     }
 
@@ -87,20 +87,22 @@ class EventsControllerTest extends TestCase
         $event = $this->createEvent(); 
 
         $now = Carbon::now();
-        $startTime = $now->format('Y-m-d H:i:s');
-        $endTime = $now->addHour()->format('Y-m-d H:i:s');
+        $startTime = $now->format('Y-m-d H:i');
+        $endTime = $now->addHour()->format('Y-m-d H:i');
+        $recurring_until = $now->addHour()->format('Y-m-d');
 
         $token = Str::random(40);
 
         $params = [
-            '_method' => 'PUT',
+            '_method' => 'PUT', 
             '_token' => $token,
             'room_id' => 1,
             'user_id' => 1,
-            'title' => 'edit book test',
+            'title' => 'afsdfasdfasasdfasd',
             'start_time' => $startTime,
             'end_time' => $endTime,
-            'description' => 'edit description test'
+            'description' => '',
+            'recurring_until' => $recurring_until,
         ];
         $response = $this->put("/admin/events/{$event->id}", $params);
 
