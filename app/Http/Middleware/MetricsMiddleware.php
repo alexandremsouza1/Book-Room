21859f771bd7900317c03f8719773d66402ae0ff
Alexandre Magno <xandemag@gmail.com>
2024-09-25 09:24:28 -0300

    add: metrics

--- app/Http/Middleware/MetricsMiddleware.php
+++ app/Http/Middleware/MetricsMiddleware.php
@@ -8,6 +8,7 @@ use Prometheus\CollectorRegistry;
 class MetricsMiddleware
 {
     private $histogram;
+    private $counter; // Adicionando o contador
 
     public function __construct()
     {
@@ -20,6 +21,14 @@ class MetricsMiddleware
             ['method', 'route'], 
             [0.01, 0.05, 0.1, 0.5, 1, 2, 5]
         );
+
+        // Contador para status 200
+        $this->counter = $registry->getOrRegisterCounter(
+            '',
+            'http_request_total',
+            'Total number of HTTP requests',
+            ['method', 'route', 'status_code']
+        );
     }
 
     public function handle($request, Closure $next)
@@ -30,7 +39,13 @@ class MetricsMiddleware
 
         $duration = microtime(true) - $startTime;
 
+        // Registrando duração
         $this->histogram->observe($duration, [$request->method(), $request->path()]);
+
+        // Incrementando contador para status 200
+        if ($response->getStatusCode() === 200) {
+            $this->counter->inc([$request->method(), $request->path(), '200']);
+        }
 
         return $response;
     }
